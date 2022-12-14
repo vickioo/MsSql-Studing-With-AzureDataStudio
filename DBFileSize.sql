d4699f5a48d09ed40403e13db2aa5c5a116c380a
vickioo <jackljl007@hotmail.com>
2022-12-14 11:45:07 +0800

    🔧 Add more origin data output & formatting

--- DBFileSize.sql
+++ DBFileSize.sql
@@ -1,12 +1,17 @@
 CREATE TABLE #FileSize
-(dbName NVARCHAR(128), 
-    FileName NVARCHAR(128), 
-    type_desc NVARCHAR(128),
-    CurrentSizeMB DECIMAL(10,2), 
-    FreeSpaceMB DECIMAL(10,2)
+(
+    dbName        NVARCHAR(128),
+    FileName      NVARCHAR(128),
+    type_desc     NVARCHAR(128),
+    CurrentSizeMB DECIMAL(10,2),
+    FreeSpaceMB   DECIMAL(10,2),
+    FileId        INT,
+    MaxSize       INT,
+    Path          NVARCHAR(250)
 );
-    
-INSERT INTO #FileSize(dbName, FileName, type_desc, CurrentSizeMB, FreeSpaceMB)
+
+INSERT INTO #FileSize
+    (dbName, FileName, type_desc, CurrentSizeMB, FreeSpaceMB, FileId, MaxSize, Path)
 exec sp_msforeachdb 
 'use [?]; 
  SELECT DB_NAME() AS DbName, 
@@ -14,12 +19,13 @@ exec sp_msforeachdb
         type_desc,
         size/128.0 AS CurrentSizeMB,  
         size/128.0 - CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT)/128.0 AS FreeSpaceMB
+        , file_id, max_size, physical_name
 FROM sys.database_files
 WHERE type IN (0,1);';
-    
-SELECT * 
+
+SELECT *
 FROM #FileSize
 WHERE dbName NOT IN ('distribution', 'master', 'model', 'msdb')
-AND FreeSpaceMB > 1;
-    
+    AND FreeSpaceMB > 1;
+
 DROP TABLE #FileSize;
