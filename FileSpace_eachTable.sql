95886dae294befc3a37df4c9b08f1dbea0784bbd
vickioo <jackljl007@hotmail.com>
2022-12-14 11:24:10 +0800

    🎨 formatting with [Align Col Definitions In Cols]

--- FileSpace_eachTable.sql
+++ FileSpace_eachTable.sql
@@ -2,13 +2,14 @@
 if exists(select 1 from tempdb..sysobjects where id=object_id('tempdb..#test') and xtype='u')
 drop table #test
 go
-create table #test( 
-name varchar(50), 
-rows int, 
-reserved varchar(20), 
-data varchar(20), 
-index_size varchar(20), 
-unused varchar(20) 
+create table #test
+(
+    name       varchar(50),
+    rows       int,
+    reserved   varchar(20),
+    data       varchar(20),
+    index_size varchar(20),
+    unused     varchar(20)
 )
  
 insert into #test 
